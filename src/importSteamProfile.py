d7bf83630810bb9de31479862bbc064a9c0b8a0e
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-03-17 14:04:44 +0100

    add steamprofile requests

--- src/importSteamProfile.py
+++ src/importSteamProfile.py
@@ -11,12 +11,12 @@ def getGroupfromS3():
     print(os.environ['S3_BUCKET'])
     response = s3client.get_object(
         Bucket=os.environ['S3_BUCKET'],
-        Key=os.environ['S3_OBJECT_STEAM_GROUP_PATH']+os.environ['STEAMGROUPID']+'.json',
+        Key=os.environ['S3_OBJECT_STEAM_GROUP_PATH']+os.environ['STEAM_GROUP_ID']+'.json',
     )
     return response
 
 def getGroup():
-    url = 'https://'+os.environ['S3_BUCKET']+'.sos-'+os.environ['S3_REGION']+'.'+os.environ['S3_ENDPOINT']+os.environ['S3_OBJECT_STEAM_GROUP_PATH']+os.environ['STEAMGROUPID']+'.json'
+    url = 'https://'+os.environ['S3_BUCKET']+'.sos-'+os.environ['S3_REGION']+'.'+os.environ['S3_ENDPOINT']+os.environ['S3_OBJECT_STEAM_GROUP_PATH']+os.environ['STEAM_GROUP_ID']+'.json'
     print(url)
     url='https://sos-ch-dk-2.exo.io/gameclubch/api/rest/v1/steam/group/103582791430857185.json'
     response = urllib.request.urlopen(url).read()
@@ -24,6 +24,17 @@ def getGroup():
     return data['memberList']['members']['steamID64']
 
 ## Load Steam Profile from API
+def getSteamProfile(steamID):
+    url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+os.environ['STEAM_API_KEY']+'&steamids='+steamID
+    response = urllib.request.urlopen(url).read()
+    data = json.loads(response)
+    return data['response']['players'][0]
+
+def loadSteamProfilefromGroup(group):
+    for member in group:
+        print(member)
+        profile = getSteamProfile(member)
+        print(profile)
 
 ## Update Steam Profile in S3 Storage
 
@@ -33,7 +44,8 @@ def __main__():
     print("Import SteamProfile")
     print("Load Members from Group")
     group = getGroup()
-    print(group)
+    #print(group)
+    loadSteamProfilefromGroup(group)
 
 
 if __name__== "__main__":
