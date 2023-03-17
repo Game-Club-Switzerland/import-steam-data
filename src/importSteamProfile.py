2eb02df6d296a382f2a24c7303471715848ce548
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-03-17 15:30:56 +0100

    remove commants and no need functions

--- src/importSteamProfile.py
+++ src/importSteamProfile.py
@@ -2,19 +2,10 @@ import os
 from dotenv import find_dotenv, load_dotenv
 import json
 import urllib
-from s3client import s3client, dictToS3
+from s3client import dictToS3
 import time
 
 load_dotenv(find_dotenv())
-
-## Load Memberlist from S3 Storage
-def getGroupfromS3():
-    print(os.environ['S3_BUCKET'])
-    response = s3client.get_object(
-        Bucket=os.environ['S3_BUCKET'],
-        Key=os.environ['S3_OBJECT_STEAM_GROUP_PATH']+os.environ['STEAM_GROUP_ID']+'.json',
-    )
-    return response
 
 def getGroup():
     url = 'https://'+os.environ['S3_BUCKET']+'.sos-'+os.environ['S3_REGION']+'.'+os.environ['S3_ENDPOINT']+'/'+os.environ['S3_OBJECT_STEAM_GROUP_PATH']+os.environ['STEAM_GROUP_ID']+'.json'
@@ -39,13 +30,8 @@ def getSteamProfilefromGroup(group):
         groupDict[member]=(profile)
         dictToS3(profile, os.environ['S3_BUCKET'], os.environ['S3_OBJECT_STEAM_PROFILE_PATH']+member+'.json',)
         i=i+1
-        time.sleep(0.75)
-
+        time.sleep(1)
     dictToS3(groupDict, os.environ['S3_BUCKET'], os.environ['S3_OBJECT_STEAM_PROFILE_PATH']+'index.json',)
-
-## Update Steam Profile in S3 Storage
-
-## Update Play List with Player to S3 Storage
 
 def __main__():
     print("Import SteamProfile")
